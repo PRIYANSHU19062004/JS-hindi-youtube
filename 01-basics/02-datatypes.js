c51f840576bf41d07da23375dea99d5e9df74660
PRIYANSHU19062004 <princefarswan1609@gmail.com>
2025-08-06 11:09:30 +0000

    datatypes

---
+++ 01-basics/02-datatypes.js
@@ -0,0 +1,15 @@
+"use strict"; //treat all JS code as newer version //What is strict mode
+//alert(3+3) //We are using node js not browser
+console.log(3
+    +3   //code redability should be high
+)
+let state=null
+let name="hitesh"//string datatype //It can be in single and double quote both
+let age=18 //number datatype 2 to power of 53
+let isLogged=false //boolean datatype //true and false
+//bigint for storing large numbere in trading ,stock market etc.
+ //null is a standalone value.It is a special type.null is a representation of empty value.Its type is object
+ //undefined//variable declared but not given any value//type of undefined is undefined
+//symbol=>used for uniqueness
+//object
+console.log(undefined)
