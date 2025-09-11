bbcea96bca629d4fc18bc17350281ade8d257d33
PRIYANSHU19062004 <princefarswan1609@gmail.com>
2025-09-11 03:08:50 +0000

    11-09-2025.commit

---
+++ 04-control-flow/one.js
@@ -0,0 +1,26 @@
+//if
+const isUserLoggedIn=true
+const temperature=41
+if(2=="2"){
+    console.log("executed") //will execute but 2 is not equal to "2"
+ 
+}
+if(2==="2"){ //Strict checking
+    console.log("executed") //will not print
+}
+//=== checks type first and then compares the values.
+// if(temperature===41){
+//     console.log("less than 50")
+// }
+// else{
+//     console.log("temperature is greater than 50")
+// }
+// const score=200
+// if(score>100){
+//     const power="fly"
+//     console.log(`User power: ${power}`)
+// }
+//console.log(`User power: ${power}`) will give error as power  is const so it has a local scope.
+const balance=1000
+if(balance>500)console.log("test") //Implicit scope
+if(balance>500)console.log("test"),console.log("test2") //Not recommended at all
