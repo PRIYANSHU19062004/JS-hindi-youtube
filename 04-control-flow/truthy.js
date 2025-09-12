917d438370759095780a784f05034a71ac4b343b
PRIYANSHU19062004 <princefarswan1609@gmail.com>
2025-09-12 02:41:35 +0000

    12-09-2025.commit

---
+++ 04-control-flow/truthy.js
@@ -0,0 +1,42 @@
+const userEmail="h@hitesh.ai"
+if(userEmail){ //Here we didn't do any comparision
+    console.log("Got user email")
+}
+else{
+    console.log("Don't have user email")
+}
+const user1Email=""
+if(user1Email){// as user1Email is empty string so else statement will execute.
+    console.log("Got user email")
+}
+else{
+    console.log("Don't have user email")
+}
+
+//++++++++++++++++++++++++++++++++++
+//falsy values-false,0,-0,BigInt 0n,"",null,undefined,NaN(Not a Number)
+//truthy  values- "0",'flase'," ",[],{}empty object,function(){}empty function
+const honda=[]
+// if(honda.length===0){
+//     console.log("Array is empty")
+// }
+const emptyObj={}
+if(Object.keys(emptyObj).length===0){
+    console.log("Object is empty")
+}
+
+//++++++++++++++++++++++++++++++//
+//Nullish Coalescing Operator(??):null undefined
+let val1;
+//val1=5??10 //5
+//val1=null??10 //10
+//val1=undefined??//15
+val1=null??10??20 //first value 10 will be assigned.
+
+console.log(val1);
+
+//Terniary Operator
+//condition?true:false
+const iceTeaPrice=100
+iceTeaPrice<=80?console.log("less than 80"):console.log("more than 80") //more than 80
+
