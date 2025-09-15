127e08ef440ddde5c4f386fc42a344fca7457da3
PRIYANSHU19062004 <princefarswan1609@gmail.com>
2025-09-15 16:29:08 +0000

    15-09-2025

---
+++ 05-iterations/three.js
@@ -0,0 +1,15 @@
+//for of loop
+//We also put objects inside array and at each interation we derive some values from those objects.
+// [{},{},{}]
+const arr=[1,2,3,4,5]
+for (const num of arr) {
+    console.log(num);
+    
+}
+const greetings="Hello World!"
+for(const greet of greetings){
+    console.log(`Each char is ${greet}`)
+}
+//Maps
+const map=new Map()      // Map is an object in itself
+
