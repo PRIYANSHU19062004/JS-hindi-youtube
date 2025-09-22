ff149333523aa0ee7814e3b3ac8f7f829f842df4
PRIYANSHU19062004 <princefarswan1609@gmail.com>
2025-09-22 06:42:52 +0000

    22-09-2025.commit

--- 05-iterations/eight.js
+++ 05-iterations/eight.js
@@ -1,9 +1,36 @@
-//reduce
+//reduce.It is used in shopping carts.
 //read from mdn documentation.Accumulator stores some values.Current value is the item from the array.
 
-const myNums=[1,2,3]
-const myTotal=myNums.reduce(function(acc,currval){
-    console.log(`acc:${acc} and currval: ${currval}`)
-    return acc + currval
-},0) //accumulator is first passed 0.After accumulator stores the sum of accumulator value and current value.
-console.log(myTotal);
+ const myNums=[1,2,3]
+// const myTotal=myNums.reduce(function(acc,currval){ //currval contains the value of the current element in the array.On the first call,its value is array[0] if initial value is specified;otherwise its value is array[1].
+//     console.log(`acc:${acc} and currval: ${currval}`)
+//     return acc + currval
+// },0) //accumulator is first passed 0.After accumulator stores the sum of accumulator value and current value.
+// console.log(myTotal);
+
+//reduce in arrow function
+// const myTotal=myNums.reduce((acc,curr)=>acc+curr,0)
+// console.log(myTotal)
+
+
+const shoppingCart=[
+{
+    itemName:"js course",
+    price:2999
+},
+{
+    itemName:"python course",
+    price:999
+},
+{
+    itemName:"mobile development course",
+    price:6999
+},
+{
+    itemName:"data science course",
+    price:12999
+},
+]
+//Add all the prices in the shopping cart.
+const priceToPay=shoppingCart.reduce((acc,item)=>acc+item.price,0)
+console.log(priceToPay);
