d2850263dd81b73589c7c5e91ef0cf0982c0ada7
PRIYANSHU19062004 <princefarswan1609@gmail.com>
2025-09-24 07:25:40 +0000

    24-09-2025.commit

--- 02-basics/01-arrays.js
+++ 02-basics/01-arrays.js
@@ -2,7 +2,7 @@
 //Arrays in JavaScript are resizable.They can be mix of datatypes.
 //JavaScript arrays are not associative arrays and so,array elements cannot be accessed using arbitrary strings as indexes.
 //JavaScript array-copy_operations create shallow copies.
-//A shallow copy of and object is a copy whose properties share the same references(point to the same underlying values)as those of the source object from which the copy was made.Any change to the copy lead to the same change in the original array
+//A shallow copy of an object is a copy whose properties share the same references(point to the same underlying values)as those of the source object from which the copy was made.Any change to the copy lead to the same change in the original array
 //A deep copy of an object is a copy whose properties don't share the same references as those of the source object.
 const myArr=[0,1,3,5,34,5]
 const myHeroes=["shaktiman","naagraj"]
