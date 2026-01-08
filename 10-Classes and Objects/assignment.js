e86f1ac5ad244adb952144c513064eccd0a77804
PRIYANSHU19062004 <princefarswan1609@gmail.com>
2026-01-08 10:21:06 +0000

    08-01-2026.commit

--- 10-Classes and Objects/assignment.js
+++ 10-Classes and Objects/assignment.js
@@ -10,3 +10,13 @@ class User{
 }
 let student1=new User("Priyanshu","princefarswan1609@gmial.com");
 let student2=new User("abhai","afafbafaf.@gmail.com");
+let teacher1=new User("Dean","dean@gsmail.com")
+class Admin extends User{
+    constructor(name,email){
+        super(name,email); //Calling the constructor of parent class "User" and passing the values emial and name.
+    }
+    editData(){
+        DATA="Some new value";
+    }
+}
+let admin1=new Admin("admin","admin@college12.com");
---
+++ 10-Classes and Objects/try-catch.js
@@ -0,0 +1,16 @@
+let a=5;
+let b=10;
+console.log("a=",a);
+console.log("b=",b);
+console.log("a+b=",a+b);
+try{
+    console.log("a+b=",a+c);//error   //As error strikes,the code doesn't stop executing ,it continues executing.
+}catch(err){
+    console.log(err);
+}
+console.log("a+b=",a+b);
+console.log("a+b=",a+b);
+console.log("a+b=",a+b);
+console.log("a+b=",a+b);
+console.log("a+b=",a+b);
+console.log("a+b=",a+b);
